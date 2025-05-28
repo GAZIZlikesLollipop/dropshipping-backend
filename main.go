a36c1bc8111d22cd40e8801ae63c320e589bd284
lollipop <gazizvalitvo230378@gmail.com>
2025-05-28 09:52:06 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	// _ "github.com/mattn/sqlite3"
+)
+
+type Product struct {
+	Id    int64  `json:"id"`
+	Name  string `json:"name" binding:"required"`
+	Price int    `json:"price" binding:"required"`
+	Iamge string `json:"image" binding:"required"`
+}
+
+type User struct {
+	Id        int64   `json:"id"`
+	Name      string  `json:"name" binding:"required"`
+	Latitude  float64 `json:"latitude" binding:"required"`
+	Longitude float64 `json:"longitude" binding:"required"`
+	Is_card   bool    `json:"is_card"`
+	Cart      []int64 `json:"cart"`
+}
+
+var db *sql.DB
+
+func main() {
+	r := gin.Default()
+	db, err := sql.Open("sqlite3", "shop.db")
+	if err != nil {
+		log.Fatal("Ошибка создания базы данных")
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Ошибка подключения базы данных")
+	}
+
+	productTable := `
+		CREATE TABLE products (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			price INTEGER,
+			iamge TEXT NOT NULL
+		)
+	`
+	_, err = db.Exec(productTable)
+	if err != nil {
+		log.Fatal("Ошибка создания таблицы продуктов")
+	}
+
+	userTable := `
+		CREATE TABLE users (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name TEXT NOT NULL,
+			latitude REAL,
+			longitude REAL,
+			is_card INTEGER DEFAULT 0,
+			cart TEXT 
+		)
+	`
+	_, err = db.Exec(userTable)
+	if err != nil {
+		log.Fatal("Ошибка создания таблицы пользователей")
+	}
+
+	r.Run(":8080")
+}
